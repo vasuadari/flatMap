b1f3bfaf1814bbc465399c781796251893f8e04d
Vasu Adari <vasuakeel@gmail.com>
2015-01-17 18:45:12 +0530

    Added an option to accept callbacks

--- flatten.js
+++ flatten.js
@@ -1,4 +1,4 @@
-Array.prototype.flatten = function() {
+Array.prototype.flatMap = function(callback) {
   var inputArray = this;
   var start = function(value, index, length) {
     flatEach(value.shift());
@@ -7,7 +7,7 @@ Array.prototype.flatten = function() {
   }
 
   var flatEach = function(eachValue) {
-    return Array.isArray(eachValue) ? start(eachValue, 0, eachValue.length) : inputArray.push(eachValue);
+    return Array.isArray(eachValue) ? start(eachValue, 0, eachValue.length) : inputArray.push(callback(eachValue));
   }
 
   return start(inputArray, 0, inputArray.length);
